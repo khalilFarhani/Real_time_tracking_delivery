7aeba0c4384877e98d59c9144713ed07298b3654
khalilFarhani <khalilfarhany@gmail.com>
2025-06-18 10:57:14 +0100

    update

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 3000,
     },
-    base: '/',
+    base: '/horizon',
     build: {
         rollupOptions: {
             external: [],
